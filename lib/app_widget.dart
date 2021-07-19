2c66a67a27fb93d0d6f26866b82404511d0da3a5
Lucas <lucaswilliam133@gmail.com>
2021-07-19 11:21:40 -0300

    Create layout barcode scanner

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
-
 import 'modules/home/home_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+        SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitDown,
+        DeviceOrientation.portraitUp,
+      ],
+    );
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Pay Flow',
-        theme: ThemeData(primaryColor: AppColors.primary),
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          primaryColor: AppColors.primary,
+        ),
         home: LoginPage(),
         initialRoute: "/splash",
         routes: {
           "/splash": (context) => SplashPage(),
           "/home": (context) => HomePage(),
           "/login": (context) => LoginPage(),
-          "/barcode_scanner" : (context) => BarcodeScannerPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
         });
   }
 }
--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/bottom_sheet_widget/bottom_sheet_widget.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
 class BarcodeScannerPage extends StatefulWidget {
   const BarcodeScannerPage({Key? key}) : super(key: key);
@@ -14,51 +16,52 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Escaneie o código de barras do boleto",
-            style: TextStyles.buttonBackground),
-        leading: BackButton(
-          color: AppColors.background,
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          )),
-          Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.transparent,
-              )),
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          ))
-        ],
-      ),
-      bottomNavigationBar: Container(
-        height: 56,
-        child: Row(
-          children: [
-            Expanded(
-              child: LabelButton(
-                label: "Inserir o código do boleto",
-                onPressed: () {},
+    return Material(
+      child: BottomSheetWidget(
+          primaryLabel: "Escanear novamente",
+          primaryOnPressed: () {},
+          secondaryLabel: "Digitar código",
+          secondaryOnPressed: () {},
+          title: "Não foi possível indentificar um código de barras.",
+          subTitle:
+              "Tente escanear novamente ou digite o código do seu boleto."),
+    );
+    return SafeArea(
+      top: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              title: Text("Escaneie o código de barras do boleto",
+                  style: TextStyles.buttonBackground),
+              leading: BackButton(
+                color: AppColors.background,
               ),
             ),
-            DividerVerticalWidget(),
-            Expanded(
-              child: LabelButton(
-                label: "Adicionar da galeria",
-                onPressed: () {},
-              ),
+            body: Column(
+              children: [
+                Expanded(
+                    child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                )),
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.transparent,
+                    )),
+                Expanded(
+                    child: Container(
+                  color: Colors.black.withOpacity(0.6),
+                ))
+              ],
             ),
-          ],
-        ),
+            bottomNavigationBar: SetLabelButtons(
+              primaryLabel: "Inserir código do boleto",
+              primaryOnPressed: () {},
+              secondaryLabel: "Adicionar da galeria",
+              secondaryOnPressed: () {},
+            )),
       ),
     );
   }
